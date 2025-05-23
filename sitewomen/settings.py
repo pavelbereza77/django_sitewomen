aaea741124920ce533936bcc27f75f353c5318b9
pavel <rabotam2013@yandex.ru>
2025-05-23 18:28:13 +0300

    add docker compose

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -18,14 +18,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+import os
+from os import environ
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ywt^i-jce1cq%5q4s^p0130-h9x9vdmvmge-vf8g+htkga+=71'
-
+# SECRET_KEY = 'django-insecure-ywt^i-jce1cq%5q4s^p0130-h9x9vdmvmge-vf8g+htkga+=71'
+SECRET_KEY = environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+# ALLOWED_HOSTS = ['127.0.0.1']
+
+ALLOWED_HOSTS = environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 INTERNAL_IPS = ["127.0.0.1"]
 
 
@@ -91,21 +94,30 @@ WSGI_APPLICATION = 'sitewomen.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sitewomen_db',
-        'USER': 'sitewomen',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': environ.get('DATABASE_NAME'), # 'sitewomen_db',
+        'USER': environ.get('DATABASE_USERNAME'), # 'sitewomen',
+        'PASSWORD': environ.get('DATABASE_PASSWORD'), # '1234',
+        'HOST': environ.get('DATABASE_HOST'), # 'localhost',
+        'PORT': environ.get('DATABASE_PORT'), # 5432,
+    }
+}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sitewomen_db',
+#         'USER': 'sitewomen',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#     }
+# }
 
 CACHES = {
     "default": {
@@ -149,9 +161,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
